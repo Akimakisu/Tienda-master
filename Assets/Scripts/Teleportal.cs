d8df0577e36eb9f0543e176e61681ce14f7d6f99
Akimakisu <davidmesco@hotmail.com>
2017-10-31 17:34:28 -0500

    :c

---
+++ Assets/Scripts/Teleportal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportal : MonoBehaviour {
+
+	[SerializeField] private Cubo cubo;
+	[SerializeField]GameObject tpLlegada;
+
+	[SerializeField] private InterfazGrafica inter;
+
+	void OnTriggerEnter(Collider other){
+
+		Debug.Log ("tocado");
+		if (inter.GetPodertp()) {
+			if (other.gameObject.tag == "Cubo") {
+
+				cubo.transform.position = tpLlegada.transform.position;
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
